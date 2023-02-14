1465b8e017ea1c32813c1cf6a266d820c9cebc9b
Odinson96 <egk090@gmail.com>
2023-02-15 03:10:17 +0400

    #2

--- src/components/login_1/login_1.js
+++ src/components/login_1/login_1.js
@@ -1,86 +1,142 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import './login_1.css'
+import { redirect } from "react-router-dom"
 
 
 const Login_1 = () => {
-    const [type, setType] = useState('password')
+    const [type, setType] = useState('password');
     const value = [];
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [Errors, setErrors] = useState('');
-
-    const [showPassImg, setShowPassImg] = useState(<label>&#128272;</label>)
+    const [EmailCheck, setEmailCheck] = useState('');
+    const [disable, setDisable] = useState(true);
+    const [PasswordCheck, setPasswordCheck] = useState('');
+    const [showPassImg, setShowPassImg] = useState(<label>&#128272;</label>);
+    const [scale, setScale] = useState('scale(1)');
+    const [submit, setSubmit] = useState("SUBMIT");
+    const [user, setUser] = useState({
+        "response": ""
+    });
+    var response;
 
     const typeChanger = () => {
         if (type === 'password') {
-            setType('text')
-            setShowPassImg(<i>&#128064;</i>)
+            setType('text');
+            setShowPassImg(<i>&#128064;</i>);
         } else {
-            setType('password')
-            setShowPassImg(<label>&#128272;</label>)
+            setType('password');
+            setShowPassImg(<label>&#128272;</label>);
         }
     }
+
+    useEffect(() => {
+        response = (Object.values(user.response)).join("");
+        if (response == "true") {
+            alert('ok');
+            return redirect('/profile')
+        } if (response == "") {
+
+        }
+        else {
+            setPasswordCheck(<h6 className='Errors'>
+                Incorrect login or password
+            </h6>);
+            setPassword('')
+            response = ""
+        }
+    }, [user])
 
     const handleSubmit = () => {
         if (password.length > 1 && email.length > 1) {
             value.push(email, password);
-            alert(JSON.stringify(value, null, 2));
-            value.splice(0, 2);
-            setEmail('')
-            setPassword('')
+            fetch('http://localhost:3002/user', {
+                method: 'POST',
+                body: JSON.stringify(value, null, 2)
+            })
+            value.length = 0;
+            const fetchData = async () => {
+                const result = await axios(
+                    'http://localhost:3002/user',
+                );
+                setUser(result.data);
+            };
+            fetchData();
         } else {
-            setPasswordCheck(<h6 className='Errors'>Incorrect</h6>)
+            setPasswordCheck(<h6 className='Errors'>Incorrect</h6>);
         }
     }
 
     const handleChange_email = (event) => {
-        setEmail(event.target.value)
+        setEmail(event.target.value);
     }
 
     const handleChange_password = (event) => {
-        setPassword(event.target.value)
+        setPassword(event.target.value);
     }
 
-    const [PasswordCheck, setPasswordCheck] = useState('')
+
 
     const PasswordBlur = (event) => {
         const valid = event.target.value;
         const regexp = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g;
         if (valid.length < 1) {
-            setPasswordCheck(<h6 className='Errors'>Check your password</h6>)
-            setDisable(true)
+            setPasswordCheck(<h6 className='Errors'>Check your password</h6>);
+            setDisable(true);
         } if (regexp.test(valid) === false) {
+            setDisable(true);
             setPasswordCheck(<h6 className='Errors'>
                 Password must consist of lowercase and uppercase characters, as well as at least one special character
-            </h6>)
+            </h6>);
         }
         else {
-            setPasswordCheck('')
-            setDisable(false)
+            setPasswordCheck('');
+            setDisable(false);
+            setSubmit('SUBMIT');
+            setScale('scale(1)');
         }
     }
 
-    const [EmailCheck, setEmailCheck] = useState('')
+
 
     const EmailBlur = (event) => {
         const valid = event.target.value;
         const regexp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
         if (valid.length < 1 || regexp.test(valid) === false) {
-            setEmailCheck(<h6 className='Errors'>Check your email</h6>)
-            setDisable(true)
+            setEmailCheck(<h6 className='Errors'>Check your email</h6>);
+            setDisable(true);
         }
         else {
-            setPasswordCheck('')
-            setEmailCheck('')
-            setDisable(false)
+            setPasswordCheck('');
+            setEmailCheck('');
+            setDisable(false);
+            setSubmit('SUBMIT');
+            setScale('scale(1)');
         }
     }
 
-    const [disable, setDisable] = useState(true)
+    const mouseEnter = () => {
+        if (disable) {
+            setSubmit("You won't make it.");
+            setScale('scale(1.5)');
+            setTimeout(() => {
+                setScale('scale(-1)')
+            }, 350);
+
+        }
+    }
+
+    const mouseOver = () => {
+        if (disable) {
+            if (scale === 'scale(-1)')
+                setScale('scale(1)')
+        }
+    }
 
     return (
         <div className='login_1_all'>
-            <h2 className='h2_login_1'>Login_1</h2>
+            <h2 className='h2_login_1'>Do not enter!!!</h2>
             <div className='login_enter_field'>
                 <input type='email'
                     id='email'
@@ -106,7 +162,9 @@ const Login_1 = () => {
                 <button className='show_password_button' onClick={typeChanger}>{showPassImg}</button>
             </div>
             <div className='errors_div'>{PasswordCheck}</div>
-            <button disabled={disable} className='submit_button' type='submit' onClick={handleSubmit}>SUBMIT</button>
+            <div onMouseEnter={mouseEnter} onMouseLeave={mouseOver}>
+                <button disabled={disable} style={{ transform: scale }} className='submit_button' type='submit' onClick={handleSubmit}>{submit}</button>
+            </div>
             <hr className='hr_login_1' />
             <a href='#' className='Button_no_account'>No account? <br />
                 Sign up now! </a>
